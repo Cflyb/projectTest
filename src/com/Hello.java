d71c28acb261d9710dc8bfb218ff98317779a629
qingtianzhu <1282288530@qq.com>
2020-09-11 11:28:46 +0800

    提交hello

---
+++ src/com/Hello.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @program: gittest
+ * @description:
+ * @author: 擎天柱
+ * @create: 2020-09-11 11:24:42
+ **/
+public class Hello {
+    public static void main(String[] rags){
+        System.out.println("hello,world");
+    }
+}
